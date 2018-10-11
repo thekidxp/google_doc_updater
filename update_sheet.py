c73f95cb2f56a4f7d8cac687dd7c965a33b6fe02
Stewart Wilson <thekidxp@gmail.com>
2018-10-11 01:36:20 -0400

    finally got the lambda function working need to finish FE

--- update_sheet.py
+++ update_sheet.py
@@ -1,13 +1,11 @@
-from __future__ import print_function
-
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 private_key = ''
 email = ''
-uri = ''
+uri = 'https://oauth2.googleapis.com/token'
+sheet_id = '1Ejmo4SPTuAtOvy96RYWhKpFVUPaaWJ5NHRms-VaYm_g'
 
 
 def update_handler(event, context):
@@ -17,15 +15,19 @@ def update_handler(event, context):
         "token_uri": uri,
     }
     credentials = service_account.Credentials.from_service_account_info(service_info, scopes=SCOPES)
-    sheets_service = build('sheets', 'v4', credentials=credentials)
+    sheets_service = build('sheets', 'v4', credentials=credentials, cache_discovery=False)
 
     spreadsheet_id = sheet_id
     range_name = 'Sheet1'
     body = {
-        'values': [['this', 'is', 'a', 'test']]
+        'values': [[event['key1'], event['key2'], event['key3'], event['key4']]]
     }
 
-    result = sheets_service.spreadsheets().values().append(spreadsheetId=spreadsheet_id,
+    return sheets_service.spreadsheets().values().append(spreadsheetId=spreadsheet_id,
                                                            range=range_name,
                                                            valueInputOption='RAW',
                                                            body=body).execute()
+
+
+if __name__ == '__main__':
+    update_handler('', '')
